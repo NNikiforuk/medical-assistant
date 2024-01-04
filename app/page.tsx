72f3d06da51fe94c8ed419f85e2e239e99767d34
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-01-04 18:52:44 +0100

    Changing button colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./page.module.scss";
 
 export default function Home() {
-	return <main className={styles.main}></main>;
+	const [mounted, setMounted] = useState(false);
+
+	//Hydration error with LastPass
+	useEffect(() => setMounted(true), []);
+	if (!mounted) return null;
+
+	return (
+		<main className={styles.main}>
+			<div className={styles.main_logo}>LOGO</div>
+			<div className={styles.main_links}>
+				<div className={styles.link}>SING IN</div>
+				<div className={styles.link}>SIGN UP</div>
+			</div>
+			<form className={styles.main_form}>
+				<div className={styles.main_form_item}>
+					<label>Email</label>
+					<input type="text" name="username" required />
+				</div>
+				<div className={styles.main_form_item}>
+					<label>Password</label>
+					<input type="password" name="password" required />
+				</div>
+			</form>
+		</main>
+	);
 }
