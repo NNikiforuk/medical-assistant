946cbf1faf31cb70c40fedc5ce618b3a945ee3ba
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-01-27 20:40:35 +0100

    Placeholder data for login form

---
+++ data/placeholders.js
@@ -0,0 +1,11 @@
+const users = [
+	{
+		id: "410544b2-4001-4271-9855-fec4b6a6442a",
+		login: "balonik",
+		password: "balonik123",
+	},
+];
+
+module.exports = {
+	users,
+};
---
+++ data/types.ts
@@ -0,0 +1,5 @@
+export type usersTypes = {
+	id: string;
+	login: string;
+	password: string;
+};
