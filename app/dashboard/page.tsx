08a0890cbf7f29b479d7173bc98305e2e9b5ea10
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-02-01 23:21:43 +0100

    Styling for dashboard header

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import styles from "./page.module.scss";
 import Layout from "@/components/dashboard/Layout";
 import { cookies } from "next/headers";
 import { fetchUserById } from "@/database/fetch";
+import { IoMdAddCircle } from "react-icons/io";
 
 const Dashboard = async () => {
 	const cookieStore = cookies();
@@ -11,9 +12,21 @@ const Dashboard = async () => {
 	return (
 		<div className={styles.dashboard}>
 			<header className={styles.dashboard__header}>
-				<h1 className={styles.dashboard__header__title}>Welcome {user.name}!</h1>
-				<div className={styles.dashboard__header__date}>
-					{new Date().toDateString()}
+				<div className={styles.dashboard__title}>
+					<h1 className={styles.dashboard__header__title__h1}>
+						Welcome {user.name}!
+					</h1>
+					<div className={styles.dashboard__header__title__date}>
+						{new Date().toDateString()}
+					</div>
+				</div>
+				<div className={styles.dashboard__header__add__medicine}>
+					<div className={styles.dashboard__header__add__medicine__icon}>
+						<IoMdAddCircle />
+					</div>
+					<div className={styles.dashboard__header__add__medicine__desc}>
+						Add medicine
+					</div>
 				</div>
 			</header>
 			<Layout>main</Layout>
